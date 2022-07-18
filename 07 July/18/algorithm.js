b336dc863e5b58b3ed2e413df25137b9ccab21be
JasonLe <Dung2781993@gmail.com>
2022-07-18 20:09:49 +1000

    Initial project conflict

---
+++ 07 July/18/algorithm.js
@@ -0,0 +1,32 @@
+const { json } = require("body-parser");
+let jsonObject = `
+{
+    "status": false,
+    "message": "Getting value succesfully"
+    "data": [
+        {
+            "name": "Sammy1",
+            "age": 60
+        },
+        {
+            "name": "Sammy2",
+            "age": 20
+        },
+        {
+            "name": "Sammy3",
+            "age": 80
+        },
+        {
+            "name": "Sammy4",
+            "age": 40
+        }
+    ]
+}`;
+
+let bodyData = JSON.parse(jsonObject); 
+let response = bodyData.data.sort(function(a, b) {
+    (a.age < b.age) ? 1 : -1
+});
+console.log(response);
+
+
